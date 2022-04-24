98843b4c7334c6f3bd865ca95fa51141e874b946
Naman <nmndwivedi@gmail.com>
2022-04-24 06:56:25 +0200

    graphs made, livestream integration

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
   Bidwatcher,
   Approval,
@@ -6,69 +6,100 @@ import {
   TokenCreated,
   TokenListed,
   TokenSold,
-  Transfer
-} from "../generated/Bidwatcher/Bidwatcher"
-import { ExampleEntity } from "../generated/schema"
+  Transfer,
+} from "../generated/Bidwatcher/Bidwatcher";
+import { Token, User } from "../generated/schema";
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+export function handleTokenCreated(event: TokenCreated): void {
+  let user = User.load(event.params.owner.toHexString());
+  if (!user) {
+    user = new User(event.params.owner.toHexString());
+    user.save()
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  let token = Token.load(event.params.tokenId.toString());
 
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
+  if (token == null) {
+    token = new Token(event.params.tokenId.toString());
+    token.creator = event.params.owner.toHexString();
+    token.owner = event.params.owner.toHexString();
+    token.uri = event.params.uri.toHex();
+    token.bidders = [event.params.owner.toHexString()];
+  }
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.balanceOf(...)
-  // - contract.fetchItemsListed(...)
-  // - contract.fetchMarketItems(...)
-  // - contract.fetchMyNFTs(...)
-  // - contract.getApproved(...)
-  // - contract.getListingPrice(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.name(...)
-  // - contract.ownerOf(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenURI(...)
+  token.save();
 }
 
-export function handleApprovalForAll(event: ApprovalForAll): void {}
+export function handleTokenSold(event: TokenSold): void {
+  let user = User.load(event.params.buyer.toHexString());
+  if (!user) {
+    user = new User(event.params.buyer.toHexString());
+    user.save()
+  }
 
-export function handleTokenCreated(event: TokenCreated): void {}
+  let token = Token.load(event.params.tokenId.toString());
 
-export function handleTokenListed(event: TokenListed): void {}
+  if (token == null) {
+    token = new Token(event.params.tokenId.toString());
+    token.creator = event.params.buyer.toHexString();
+  }
 
-export function handleTokenSold(event: TokenSold): void {}
+  token.owner = event.params.buyer.toHexString();
+  token.bidders.push(event.params.buyer.toHexString());
+  token.save();
+}
 
-export function handleTransfer(event: Transfer): void {}
+// export function handleApproval(event: Approval): void {
+//   // Entities can be loaded from the store using a string ID; this ID
+//   // needs to be unique across all entities of the same type
+//   let entity = ExampleEntity.load(event.transaction.from.toHex())
+
+//   // Entities only exist after they have been saved to the store;
+//   // `null` checks allow to create entities on demand
+//   if (!entity) {
+//     entity = new ExampleEntity(event.transaction.from.toHex())
+
+//     // Entity fields can be set using simple assignments
+//     entity.count = BigInt.fromI32(0)
+//   }
+
+//   // BigInt and BigDecimal math are supported
+//   entity.count = entity.count + BigInt.fromI32(1)
+
+//   // Entity fields can be set based on event parameters
+//   entity.owner = event.params.owner
+//   entity.approved = event.params.approved
+
+//   // Entities can be written to the store with `.save()`
+//   entity.save()
+
+//   // Note: If a handler doesn't require existing field values, it is faster
+//   // _not_ to load the entity from the store. Instead, create it fresh with
+//   // `new Entity(...)`, set the fields that should be updated and save the
+//   // entity back to the store. Fields that were not set or unset remain
+//   // unchanged, allowing for partial updates to be applied.
+
+//   // It is also possible to access smart contracts from mappings. For
+//   // example, the contract that has emitted the event can be connected to
+//   // with:
+//   //
+//   // let contract = Contract.bind(event.address)
+//   //
+//   // The following functions can then be called on this contract to access
+//   // state variables and other data:
+//   //
+//   // - contract.balanceOf(...)
+//   // - contract.fetchItemsListed(...)
+//   // - contract.fetchMarketItems(...)
+//   // - contract.fetchMyNFTs(...)
+//   // - contract.getApproved(...)
+//   // - contract.getListingPrice(...)
+//   // - contract.isApprovedForAll(...)
+//   // - contract.name(...)
+//   // - contract.ownerOf(...)
+//   // - contract.supportsInterface(...)
+//   // - contract.symbol(...)
+//   // - contract.tokenURI(...)
+// }
+
+// export function handleApprovalForAll(event: ApprovalForAll): void {}
